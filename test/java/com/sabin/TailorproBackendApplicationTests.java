e96bc6ad137fc0fe3625e84864d7785f72f20153
Sabin Shah <168896473+Sambie619@users.noreply.github.com>
2026-03-20 15:38:57 +0530

    Add files via upload

---
+++ test/java/com/sabin/TailorproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
